af3751b48bc0267e8883aea74c7ddef3e3924c03
armandocardosoneto <armandocardos@gmail.com>
2023-09-20 17:29:09 +0000

    segundo commit

--- JAVASCRIPT/cartas.js
+++ JAVASCRIPT/cartas.js
@@ -1,19 +1,19 @@
 // const card1 = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129465&type=card";
-const card1 = "../images/card-1.jpeg";
+const card1 = "../IMAGENS/card-1.jpeg";
 // const card2 = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129711&type=card";
-const card2 = "../images/card-2.jpeg";
+const card2 = "../IMAGENS/card-2.jpeg";
 // const card3 = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129710&type=card";
-const card3 = "../images/card-3.jpeg";
+const card3 = "../IMAGENS/card-3.jpeg";
 // const card4 = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129671&type=card";
-const card4 = "../images/card-4.jpeg";
+const card4 = "../IMAGENS/card-4.jpeg";
 // const card5 = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129671&type=card";
-const card5 = "../images/card-5.jpeg";
+const card5 = "../IMAGENS/card-5.jpeg";
 // const card6 = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129671&type=card";
-const card6 = "../images/card-6.jpeg";
+const card6 = "../IMAGENS/card-6.jpeg";
 // const card7 = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129671&type=card";
-const card7 = "../images/card-7.jpeg";
+const card7 = "../IMAGENS/card-7.jpeg";
 // const card8 = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=149551&type=card";
-const card8 = "../images/card-8.jpeg";
+const card8 = "../IMAGENS/card-8.jpeg";
 
 
 const image = document.querySelector(".mainclass-image img");
