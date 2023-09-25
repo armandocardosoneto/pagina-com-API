7c79f30a2f61d4f024f45f88beec544900607001
armandocardosoneto <armandocardos@gmail.com>
2023-09-25 15:59:38 +0000

    quarto commit

--- JAVASCRIPT/api-async-magic.js
+++ JAVASCRIPT/api-async-magic.js
@@ -1,32 +1,47 @@
-const ApiUrl = "https://api.magicthegathering.io/v1/cards"
+const showCardBtn = document.querySelector(".show-card");
+const cardName = document.querySelector(".card-name");
+const cardType = document.querySelector(".card-type");
+const cardImage = document.querySelector(".card-image");
+const ApiUrl = "https://api.magicthegathering.io/v1/cards";
 
-async function getCards(_countCards, _Path) {
-    const countCards = 10;
-    const pageSize = "?pagesize=";
+showCardBtn.addEventListener("click", getCards);
+
+async function getCards() {
     const path = "cards";
-    const endpoint = ApiUrl + path + pageSize + countCards;
-
-    //https://api.magicthegathering.io/v1/cards?pageSize=10
+    // const pageSizeString = "?pageSize";
+    // const countCards = 10;
+    // const endPoint = apiUrl + path + pageSizeString + countCards;
+    const endPoint = apiUrl + path;
 
     try {
-        const response = await fetch(endpoint, {
+        const response = await fetch(endPoint, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
             }
         });
 
-        return await response.json();
-    } catch (error) {
-        console.error("error:", error);
+        const result = await response.json();
+        console.log(result);
+
+        const thirdCard = result.cards[2];
+        console.log(thirdCard);
+
+        const thirdCardName = thirdCard.name;
+        console.log(thirdCardName);
+
+        const thirdCardType = thirdCard.type;
+        console.log(thirdCardType);
+
+        const thirdCardImage = thirdCard.imageUrl;
+        console.log(thirdCardImage);
+
+        cardName.innerText = thirdCardName;
+        cardType.innerText = thirdCardType;
+        cardImage.src = thirdCardImage;
+    }
+    catch(error) {
+        console.error("Error", error);
     }
 }
-
-function listCardsInView() {
-    const cardList = getCards(5, "cards")
-        .then(function (response) {
-            console.log(response)
-        }).catch(function (err) {
-            console.log(err)
-        })
-}
+// getCards();
